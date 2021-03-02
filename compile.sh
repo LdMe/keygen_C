6d613df126b07b0905005b08844e4262103dcb7a
LdMe <dlafuente003@gmail.com>
2021-03-02 20:40:53 +0100

    output of compile es keygen by default, not test

--- compile.sh
+++ compile.sh
@@ -6,6 +6,6 @@ if [ $# -ge 1 ];
 then
 	gcc ./source/*.c -o ./bin/$1
 else
-	gcc ./source/*.c -o ./bin/test
+	gcc ./source/*.c -o ./bin/keygen
 fi
 #compile the source files
--- source/main.c
+++ source/main.c
@@ -5,10 +5,11 @@ int main(int argc, char **argv)
     if(argc < 3) {
         return (1);
     }
+
     char *pass1 =argv[1];
     char *pass2 =argv[2];
     unsigned int seed = 0;
-    //char *result = encrypt(pass1,pass2,seed); //with seed
-    char *result = encrypt(pass1,pass2); //classic
+    char *result = encrypt_alphanumeric(pass1,pass2,seed); //with seed
+    //char *result = encrypt(pass1,pass2); //classic
     printf("%s", result);
 }
